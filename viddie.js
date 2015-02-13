57e3a097c34092b990eb919bf62e8b00bfc7ce17
deadprogram <ron@hybridgroup.com>
2015-02-12 23:29:44 -0800

    Send mqtt blink messages when we see faces

--- viddie.js
+++ viddie.js
@@ -3,7 +3,12 @@ var Cylon = require("cylon");
 Cylon.robot({
   connections: {
     edison: { adaptor: 'intel-iot' },
-    opencv: { adaptor: "opencv" }
+    opencv: { adaptor: "opencv" },
+    mqtt: { adaptor: "mqtt", 
+            host: process.env.MECHANOID_MQTT_BROKER + ":" +
+                  process.env.MECHANOID_MQTT_BROKER_PORT,
+            username: process.env.MECHANOID_MQTT_USER,
+            password: process.env.MECHANOID_MQTT_PASSWORD },
   },
 
   devices: {
@@ -21,7 +26,8 @@ Cylon.robot({
       console.log("The camera is ready!");
 
       every(100, function(){                             
-        my.buzzer.turnOff();                             
+        my.buzzer.turnOff();
+        my.mqtt.publish(process.env.MECHANOID_MQTT_BASE_TOPIC + "/blink", "0");
         my.camera.readFrame();                           
       });
     });
@@ -32,6 +38,7 @@ Cylon.robot({
         return;
       }
       my.buzzer.turnOn();
+      my.mqtt.publish(process.env.MECHANOID_MQTT_BASE_TOPIC + "/blink", "1");
       my.camera.readFrame();
     });
 
